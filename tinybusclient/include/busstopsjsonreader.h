78e5c2066eb16c96353e34d2d8049b0876b0eb6b
Chan Jun Wei <chanjunweimy@gmail.com>
2015-05-16 01:27:14 +0800

    written the skeleton of busstopsjsonreader
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- tinybusclient/include/busstopsjsonreader.h
+++ tinybusclient/include/busstopsjsonreader.h
@@ -1,5 +1,27 @@
 #ifndef BUSSTOPSJSONREADER
 #define BUSSTOPSJSONREADER
 
+#include <QObject>
+
+namespace JsonParser {
+    class BusStopsJsonReader;
+}
+
+class BusStopsJsonReader : public QObject {
+    Q_OBJECT
+
+private:
+    explicit BusStopsJsonReader(QObject *parent = 0);
+    ~BusStopsJsonReader();
+
+public:
+    BusStopsJsonReader* getObject();
+    void deleteObject();
+
+private:
+    static BusStopsJsonReader* _busStopsJsonReader;
+
+};
+
 #endif // BUSSTOPSJSONREADER
 
--- tinybusclient/src/busstopsjsonreader.cpp
+++ tinybusclient/src/busstopsjsonreader.cpp
@@ -1,1 +1,22 @@
+#include "busstopsjsonreader.h"
 
+BusStopsJsonReader* BusStopsJsonReader::_busStopsJsonReader = NULL;
+
+BusStopsJsonReader::BusStopsJsonReader(QObject *parent) :
+    QObject(parent) {
+}
+
+BusStopsJsonReader::~BusStopsJsonReader() {
+}
+
+BusStopsJsonReader* BusStopsJsonReader::getObject() {
+    if (_busStopsJsonReader == NULL) {
+        _busStopsJsonReader = new BusStopsJsonReader;
+    }
+    return _busStopsJsonReader;
+}
+
+void BusStopsJsonReader::deleteObject() {
+    _busStopsJsonReader->deleteLater();
+    _busStopsJsonReader = NULL;
+}
