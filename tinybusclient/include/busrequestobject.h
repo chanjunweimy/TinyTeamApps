53140dbe6d5fa30274dfbb6f2727c5505ea1d3ef
Chan Jun Wei <chanjunweimy@gmail.com>
2015-05-17 18:10:32 +0800

    optimize busreqobj for json reading jsonreader can now read busrequestobj
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- tinybusclient/include/busrequestobject.h
+++ tinybusclient/include/busrequestobject.h
@@ -2,6 +2,8 @@
 #define BUSREQUESTOBJECT_H
 
 #include <QString>
+#include <QVector>
+#include <QMap>
 
 namespace Object {
     class BusRequestObject;
@@ -14,15 +16,33 @@ public:
 
 public:
     QString getBusStopNumber();
+    QString getBusStopName();
     QString getBusServiceNumber();
     int getNumberOfRequest();
     void setBusStopNumber(QString busStopNumber);
     void setBusServiceNumber(QString busServiceNumber);
     void setNumberOfRequest(int numberOfRequest);
+    void setBusStopName(QString busStopName);
+
+public:
+    static QVector <QString> getParams();
+    static QMap <QString, QString> getTypeForParams(QString stringType,
+                                                    QString doubleType);
 
 private:
     QString _busStopNumber;
+    QString _busStopName;
     QString _busServiceNumber;
     int _numberOfRequest;
+
+private:
+    static QVector <QString> _params;
+    static QMap <QString, QString> _typeForParams;
+
+public:
+    static const QString PARAM_BUS_STOP_NUMBER;
+    static const QString PARAM_BUS_STOP_NAME;
+    static const QString PARAM_BUS_SERVICE_NUMBER;
+    static const QString PARAM_NUMBER_OF_REQUEST;
 };
 #endif // BUSREQUESTOBJECT_H
